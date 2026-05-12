efeb4d463c8ab66457bacaf8303473183e3606c4
Chavitoxo <compaq072012@gmail.com>
2026-05-12 15:21:00 -0400

    added track panel

--- server.js
+++ server.js
@@ -61,3 +61,19 @@ app.listen(PORT, '0.0.0.0', ()=>{
     console.log(`🚀 Running on port ${PORT}`);
 
 });
+function updateClock(){
+
+    const now = new Date();
+
+    const h = String(now.getHours()).padStart(2,'0');
+    const m = String(now.getMinutes()).padStart(2,'0');
+    const s = String(now.getSeconds()).padStart(2,'0');
+
+    document.getElementById('clock').innerText =
+    `${h}:${m}:${s}`;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
